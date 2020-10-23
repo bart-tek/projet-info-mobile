8d285807ec7c7e21856e5f77a436e9674943bb0b
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-23 09:37:23 +0200

    conf tab

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -6,6 +6,16 @@ const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
+    children: [
+      {
+        path: 'tab2',
+        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+      },
+      {
+        path: 'tab3',
+        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      }
+    ]
   }
 ];
 
